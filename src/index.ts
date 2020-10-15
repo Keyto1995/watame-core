edb5da0d57eeb86d72da271d6a8f8a47044bda64
keyto1995 <keyto1995@outlook.com>
2020-10-15 10:06:38 +0800

    实现读取.json5文件;

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import JSON5 from "json5";
+const fs = require("fs").promises;
+import path from "path";
+
+async function mapDir(dir: string): Promise<string[]> {
+  const result = [];
+  const files = await fs.readdir(dir);
+  for (let i = 0; i < files.length; i++) {
+    const filename = files[i];
+    const pathname = path.join(dir, filename);
+    const stats = await fs.stat(pathname);
+    if (stats.isDirectory()) {
+      const res = await mapDir(pathname);
+      result.push(...res);
+    } else if (stats.isFile()) {
+      if ([".json5"].includes(path.extname(pathname))) {
+        result.push(pathname);
+      }
+    }
+  }
+  return result;
+}
+let fileDirectory = ".";
+mapDir(fileDirectory).then((data) => {
+  data.forEach(async (pathname) => {
+    const data = await fs.readFile(pathname, "utf-8");
+    console.log(data);
+    let j = JSON5.parse(data);
+    console.log(j);
+  });
+});
